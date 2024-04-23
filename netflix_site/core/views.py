5a6ef48e430e0aeda4ea72861d682c4132f129f0
Alfredocgn <alfredocgn@gmail.com>
2024-04-22 22:48:07 -0300

    Add to list

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -1,14 +1,120 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Movie,MovieList
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+import re
 
 # Create your views here.
 
-
+@login_required(login_url='login')
 def index(request):
-  return render(request,'index.html')
+  movies = Movie.objects.all()
+
+  context = {
+    'movies':movies,
+  }
+
+  return render(request,'index.html',context)
+
+@login_required(login_url='login')
+def movie(request,pk):
+  movie_uuid =pk
+  movie_details = Movie.objects.get(uu_id=movie_uuid)
+
+  context ={
+    'movie_details':movie_details
+  }
+  return render(request,'movie.html',context)
+
+def add_to_list(request):
+  if request.method == "POST":
+    movie_url_id = request.POST.get('movie_id')
+    uuid_pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    match = re.search(uuid_pattern,movie_url_id)
+    movie_id = match.group() if match else None
+
+    movie = get_object_or_404(Movie,uu_id = movie_id)
+    movie_list , created = MovieList.objects.get_or_create(owner_user=request.user,movie=movie)
+
+    if created:
+      response_data = {
+        'status':"success",
+        'message':'Added'
+      }
+    else:
+      response_data = {
+        'status':'info',
+        'message':'Movie already in list'
+      }
+    return JsonResponse(response_data)
+  else:
+        return JsonResponse ({
+        'status':'error',
+        'message':'Invalid request'
+      },status =400)
+    
+    
+
+def my_list(request):
+  pass
 
 def login(request):
-  return render(request,'login.html')
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username,password=password)
+
+    if user is not None:
+      auth.login(request,user) 
+      return redirect('/')  
+    else:
+      messages.info(request, 'Invalid credentials')
+      return redirect('login')
+
+  else: 
+    return render(request,'login.html')
 
 
 def signup(request):
-  return render(request,'signup.html')
+  if request.method == 'POST':
+    email = request.POST['email']
+    username = request.POST['username']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password == password2:
+      if User.objects.filter(email = email).exists():
+        messages.info(request,'Email already exists')
+        return redirect('signup')
+      elif User.objects.filter(username = username).exists():
+        messages.info(request,'Username already exists')
+        return redirect('signup')
+      else:
+        user = User.objects.create_user(username=username,email=email,password=password)
+        user.save()
+        
+        user_login = auth.authenticate(username=username,password=password)
+        auth.login(request,user_login)
+        return redirect('/')
+
+    else:
+      messages.info(request, 'Password Not Matching')
+      return redirect('/signup')
+
+
+
+  else:
+    return render(request,'signup.html')
+  
+
+
+@login_required(login_url='login')
+def logout(request):
+  auth.logout(request)
+  return redirect('login')
+
+
